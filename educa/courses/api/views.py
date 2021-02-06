f0ee48c4e62b92897ae25666f2d185557bca6676
Oswaldo <Zavala>
2021-02-06 16:29:16 -0500

    Created nested serializers

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from ..models import Subject
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 class SubjectListView(generics.ListAPIView):
